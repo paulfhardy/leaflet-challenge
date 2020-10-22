531e46ae32178f4c01265ef93c4b43c9613d7506
Paul Hardy <paulfhardy@hotmail.com>
2020-10-21 18:13:18 -0700

    logic.js updated with updated variable types

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -3,15 +3,17 @@
 // recorfed over the past week.  The data is updated every minute.
 // This program reads in the geojson data, and maps the earthquake data
 // in three ways: 
+//
 //  1) Circles represent the location (latlng) of the earthquake on the map.
 //  2) The size of the circle represents the magnitude of the earthquake.
 //  3) The color of the circle represents the depth of the earthquake.
+//
 // Program Developed and tested by : Paul Hardy
 // Program create on : 10/21/2020
 //**********************************************************************/
 
 // Store our API endpoint inside queryUrl - this is dat for the last week of activity.
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+const queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Perform a GET request to the query URL
 d3.json(queryUrl).then(data => {
@@ -22,8 +24,8 @@ d3.json(queryUrl).then(data => {
 
 function createFeatures(earthquakeData) {
 
-    // Define a function we want to run once for each feature in the features array
-    // Give each feature a popup describing the Magmitude, Depth, Place, & time of the earthquake
+    // Define a function that runs once for each feature in the features array
+    // Give each feature a popup describing the Magnitude, Depth, Place, & time of the earthquake
     function onEachFeature(feature, layer) {
       layer.bindPopup("Magnitude: " + feature.properties.mag +
         "<div>"+"Depth: " + feature.geometry.coordinates[2] + " km"+ "</div>" +
@@ -32,11 +34,11 @@ function createFeatures(earthquakeData) {
         "<hr><p>" + "Time: "+ new Date(feature.properties.time) + "</p>");
     };
     
-    // Define a variable mags that we want to run once for each feature in the features array
+    // Define a variable "mags" that we want to run once for each feature in the features array
     // to create a layer of circles for each earthquake, at the location specified. Also set the 
     // radius of the circles to represent the magnitude of the earthquake, and the color to represent
     // the depth of the earthquake.
-    var mags = L.geoJSON(earthquakeData, {
+    let mags = L.geoJSON(earthquakeData, {
     onEachFeature: onEachFeature,
     pointToLayer: (feature, latlng) => {
       return new L.Circle(latlng, {
@@ -58,14 +60,14 @@ function createFeatures(earthquakeData) {
 function createMap(mags) {
   
   // Create our map, giving it the light map layer and mags layer to display on load
-  var myMap = L.map("map", {
+  let myMap = L.map("map", {
     center: [
       0, 0],
     zoom: 3,
     layers: [mags]
    });
 
-   // Define lightmap layer and add it to the map
+   // Define light-v10 map layer and add it to the map
    L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
@@ -76,13 +78,13 @@ function createMap(mags) {
    }).addTo(myMap);
 
    // Set up the legend
-    var legend = L.control({ position: "bottomright" });
+    let legend = L.control({ position: "bottomright" });
         
     legend.onAdd = function (myMap) {
         var div = L.DomUtil.create("div", "info legend");
         depths = [0,10,30,50,70,90]
   
-    var legendInfo = `<h3>Earthquake <br> Depth (km) </h3>`;
+    let legendInfo = `<h3>Earthquake <br> Depth (km) </h3>`;
     div.innerHTML = legendInfo;
 
       for (var i = 0; i < depths.length; i++) {
